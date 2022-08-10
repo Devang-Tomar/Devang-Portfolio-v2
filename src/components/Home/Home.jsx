e12eb7b38b685b27de48f833b933e0d36d2da3a9
d0t077o <Devang.Tomar@walmart.com>
2022-08-10 17:41:41 +0530

    Adding

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,6 +12,12 @@ export default function Home() {
                     <h1
                         className="text-7xl font-extrabold"
                     >Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
+                    <div className="mt-10">
+                        <button type="button" class="text-white bg-purple-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-base px-8 py-7 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            EXPLORE WORKS
+                            <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                    </div>
                 </div>
                 <div class="relative w-full max-w-lg">
                     <div class="absolute top-0 -left-10 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
