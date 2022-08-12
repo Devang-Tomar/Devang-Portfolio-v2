4a8f251a7a27264e655d3ef062e9f046985aef78
devangtomar <devangtomar123@gmail.com>
2022-08-12 17:17:07 +0530

    Changing nav-bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ export default function Navbar() {
         <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
                 <a href="https://flowbite.com" className="flex items-center">
-                    <img
+                    {/* <img
                         src="https://flowbite.com/docs/images/logo.svg"
                         className="mr-3 h-6 sm:h-9"
                         alt="Flowbite Logo"
-                    />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                        Flowbite
+                    /> */}
+                    <span className="self-center text-3xl font-extrabold whitespace-nowrap dark:text-white">
+                        devang.
                     </span>
                 </a>
                 <button
@@ -54,7 +54,7 @@ export default function Navbar() {
                                 data-collapse-toggle="mega-menu-full-dropdown"
                                 className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                             >
-                                Company{" "}
+                                Drop-down{" "}
                                 <svg
                                     className="ml-1 w-5 h-5"
                                     fill="currentColor"
@@ -74,7 +74,7 @@ export default function Navbar() {
                                 href="#"
                                 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                             >
-                                Marketplace
+                                Lorem.
                             </a>
                         </li>
                         <li>
@@ -82,7 +82,7 @@ export default function Navbar() {
                                 href="#"
                                 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                             >
-                                Resources
+                                Lorem.
                             </a>
                         </li>
                         <li>
@@ -90,7 +90,7 @@ export default function Navbar() {
                                 href="#"
                                 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                             >
-                                Contact
+                                Lorem.
                             </a>
                         </li>
                     </ul>
