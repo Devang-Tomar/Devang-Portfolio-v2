711313e40b7728d0b7203a9739ee13189febb7c1
devangtomar <devangtomar123@gmail.com>
2022-08-12 16:04:49 +0530

    Adding personal image for index

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Navbar from "../Navbar";
+import personalimg from '../../assets/img/personal.jpg'
 
 export default function Home() {
     return (
         <>
             <Navbar></Navbar>
-            <div className="grid grid-cols-2">
-                <div className="px-52 py-40">
+            <div className="flex p-40">
+                <div className="">
                     <h1 className="mb-5 font-extrabold">CODER, DEVOPS ENGINEER, TRAVELER.</h1>
                     <h1
                         className="text-7xl font-extrabold"
                     >Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
                 </div>
-                <div>
-
+                <div className="">
+                    <img src={personalimg}></img>
                 </div>
             </div>
         </>
