a0d3f8454282aabf0ae65adac33fdd29ba8de9f0
devangtomar <devangtomar123@gmail.com>
2022-08-12 17:06:10 +0530

    Adding image background design

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,8 +13,16 @@ export default function Home() {
                         className="text-7xl font-extrabold"
                     >Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
                 </div>
-                <div className="">
-                    <img src={personalimg}></img>
+
+                <div class="relative w-full max-w-lg">
+                    <div class="absolute top-0 -left-10 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+                    <div class="absolute top-0 -right-10 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+                    <div class="absolute -bottom-20 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+                    <div class="m-8 relative space-y-4 flex flex-wrap justify-center">
+                        <div className="w-3/5 px-4">
+                            <img class="shadow-lg rounded-3xl max-w-full h-auto align-middle border-none" src={personalimg}></img>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
