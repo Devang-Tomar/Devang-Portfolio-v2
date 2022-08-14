72cde8abb4b4ec88f406be20dafcc9cc9cc83902
devangtomar <devangtomar123@gmail.com>
2022-08-14 12:36:38 +0530

    Adding grid for home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,10 +5,10 @@ export default function Home() {
     return (
         <>
             <Navbar></Navbar>
-            <div className="grid gap-10 grid-cols-2">
+            <div className="grid grid-cols-2">
                 <div>
                     <h1
-                        className="flex px-40 text-7xl font-extrabold"
+                        className="px-40 text-7xl font-extrabold"
                     >Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
                 </div>
                 <div>
